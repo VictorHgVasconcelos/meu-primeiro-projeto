faf27b04e51eec06ea76e9b551f1a39d29a0bd90
Victor Hugo Vasconcelos <victorhg.vasconcelos@gmail.com>
2022-12-01 13:09:45 -0300

    first commit

---
+++ assets/js/aula1.js
@@ -0,0 +1,7 @@
+console.log('Victor "Hugo"');//string = texto
+console.log("Victor 'Hugo'");
+console.log(`'Victor' "Hugo" `);
+
+console.log(35, 15.65, 'Victor Hugo');//number=numero, nao precisa de aspas
+
+console.log('Meu nome é "Victor Hugo". Estou aprendendo Javascript as', 10, 'da manha' );
